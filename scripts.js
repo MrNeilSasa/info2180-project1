1e84d7a7970e98de582485d5d7c9d75e87296732
Antonio Neil <bossneil20@gmail.com>
2022-10-30 23:09:00 -0500

    Added Images

--- scripts.js
+++ scripts.js
@@ -1,1 +1,30 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+
+    var msg = document.querySelector(".message");
+    console.log(msg);
+    var subscribe = document.querySelector("button");
+    console.log(subscribe);
+
+    subscribe.addEventListener('click', validate);
+
+    function validate(event){
+        event.preventDefault();
+        var validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        var mail = document.getElementById("email").value;
+        
+        
+
+        if (validRegex.test(mail)){
+            msg.textContent ='Thank you! Your email address ' + mail +' has been added to our mailing list!'
+            return true;
+            
+
+        }
+        else{
+            msg.textContent ="Please enter a valid email address."
+            return false;
+      
+        }
+    }
+}
